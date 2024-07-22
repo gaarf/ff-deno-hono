e8ddf2f24a8f99da46d7bdc258b96e039689e011
Adrien <gaarf@users.noreply.github.com>
2024-07-22 16:17:30 -0700

    argh

--- src/routes/api.tsx
+++ src/routes/api.tsx
@@ -2,15 +2,11 @@ import { Hono, isoNow} from "@/util.ts";
 
 export default new Hono()
   .get("/*", async (c) => {
-    const { time, disclaimer, bpi } = await fetch(
+    const data = await fetch(
       "https://api.coindesk.com/v1/bpi/currentprice.json"
     ).then(result => result.json());
     return c.json({
-      btc: {
-        updated: time.updated,
-        disclaimer,
-        usd: bpi.usd.rate_float
-      },
+      data,
       renderedAt: isoNow(),
     });
   });
