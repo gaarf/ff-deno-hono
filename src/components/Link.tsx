78abb42d811d7185fedf9110900eed92f3c11c5c
Adrien <gaarf@users.noreply.github.com>
2024-07-28 07:01:03 -0700

    oops

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,8 +4,8 @@ import { forwardRef } from "hono/jsx";
 
 type LinkProps = PropsWithChildren<JSX.IntrinsicElements["a"]>;
 
-export const Link = forwardRef<HTMLAnchorElement>(
-  ({ children, ...props }: LinkProps, ref) => {
+export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
+  ({ children, ...props }, ref) => {
     return (
       <a {...props} class={cn("underline", props.class)} ref={ref}>
         {children}
