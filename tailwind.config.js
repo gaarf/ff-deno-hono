819313364345166c8d2e49efd3d3d76914c80bdd
Adrien <gaarf@users.noreply.github.com>
2024-07-28 18:42:19 -0700

    rm daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-import daisyui from "daisyui";
-
 /** @type {import('npm:tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [],
 };
