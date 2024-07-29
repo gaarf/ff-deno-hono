12fc2abd51a718642f0686185b5afbd6e8a2a8fe
Adrien <gaarf@users.noreply.github.com>
2024-07-28 18:28:24 -0700

    add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import daisyui from "daisyui";
+
 /** @type {import('npm:tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
