17dfd61ac09fc5b29d01e6987474032cc33f1522
Adrien <gaarf@users.noreply.github.com>
2024-08-01 20:39:42 -0700

    autotest

--- tests/compat.test.ts
+++ tests/compat.test.ts
@@ -1,7 +1,9 @@
 import { assertEquals } from "std/assert/mod.ts";
 import HonoJsx from "hono/jsx";
 
-Deno.test("react/hono compatibility shim", async () => {
-  const { createElement } = await import("@/compat.ts");
-  assertEquals(createElement, HonoJsx.createElement);
+const { ...exports } = await import("@/compat.ts");
+Object.entries(exports).forEach(([k, x]) => {
+  Deno.test(k, () => {
+    assertEquals(x, HonoJsx[k as keyof typeof HonoJsx]);
+  });
 });
