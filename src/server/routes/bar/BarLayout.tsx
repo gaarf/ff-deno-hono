5979d651fafab889d9a5be39466e1cc3d948f727
Adrien <gaarf@users.noreply.github.com>
2024-08-01 20:24:54 -0700

    bed

--- src/server/routes/bar/BarLayout.tsx
+++ src/server/routes/bar/BarLayout.tsx
@@ -3,8 +3,8 @@ import { PropsWithChildren } from "@/utils.ts";
 export const BarLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <h1 class="font-bold">Bar layout</h1>
-      <section class="border border-yellow-500 border-dashed rounded p-4">
+      <h1 className="font-bold">Bar layout</h1>
+      <section className="border border-yellow-500 border-dashed rounded p-4">
         {children}
       </section>
     </>
