f14c25464edaa1ffdc2814ecb883527066fc0fa3
Adrien <gaarf@users.noreply.github.com>
2024-08-12 16:16:56 -0700

    fmt

--- src/client/islands/Toaster.tsx
+++ src/client/islands/Toaster.tsx
@@ -2,11 +2,14 @@ import { Toaster as ReactHotToaster } from "react-hot-toast";
 import clientOnly from "@/client/only.tsx";
 
 export const Toaster = clientOnly(() => (
-  <ReactHotToaster position="bottom-right" toastOptions={{
-    className: 'border',
-    style: {
-      background: 'rgb(var(--color-default-bg))',
-      color: 'rgb(var(--color-default-text))',
-    }
-  }} />
+  <ReactHotToaster
+    position="bottom-right"
+    toastOptions={{
+      className: "border",
+      style: {
+        background: "rgb(var(--color-default-bg))",
+        color: "rgb(var(--color-default-text))",
+      },
+    }}
+  />
 ));
