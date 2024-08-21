c92258c1949faa69c138bd87b74b9d422b6e511a
Adrien <gaarf@users.noreply.github.com>
2024-08-21 16:56:00 -0700

    add title col

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,5 +1,5 @@
 import { type Tables } from "@/supabase/schema.gen.ts";
-import { TimeAgo } from "./TimeAgo.tsx";
+import { TimeAgo } from "@/components/TimeAgo.tsx";
 
 type BlogPostProps = {
   post: Tables<"posts">;
@@ -7,7 +7,7 @@ type BlogPostProps = {
 
 export const BlogPost = ({ post }: BlogPostProps) => (
   <article className="flex flex-col items-start p-2 bg-neutral-2">
-    {/* <h2>{post.title}</h2> */}
+    <h2>{post.title}</h2>
     <blockquote>{post.content}</blockquote>
     <address>{post.author_id}</address>
     <TimeAgo when={post.created_at} unit="days" />
