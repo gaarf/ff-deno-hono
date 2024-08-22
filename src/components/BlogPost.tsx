c48e2fd4bb7bb65fc68776b77b206f576f15dc58
Adrien <gaarf@users.noreply.github.com>
2024-08-21 17:16:11 -0700

    BlogPost styled

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,15 +1,28 @@
 import { type Tables } from "@/supabase/schema.gen.ts";
 import { TimeAgo } from "@/components/TimeAgo.tsx";
+import { Link } from "@/components/intrinsic.ts";
+import { Box } from "@/components";
 
 type BlogPostProps = {
   post: Tables<"posts">;
 };
 
 export const BlogPost = ({ post }: BlogPostProps) => (
-  <article className="flex flex-col items-start p-2 bg-neutral-2">
-    <h2>{post.title}</h2>
-    <blockquote>{post.content}</blockquote>
-    <address>{post.author_id}</address>
-    <TimeAgo when={post.created_at} unit="days" />
+  <article className="bg-neutral-2 border rounded">
+    <Box className="border-b p-2 flex-col items-start md:flex-row md:items-end">
+      <h2 className="font-extrabold text-xl">{post.title}</h2>
+      <address className="not-italic text-xs text-nowrap">
+        <TimeAgo
+          className="italic"
+          when={post.created_at}
+          unit="days"
+          options={{ maximumFractionDigits: 0 }}
+        />
+        {", by "}
+        <Link href="#">{post.author_id.split("-")[0]}</Link>
+      </address>
+    </Box>
+
+    <blockquote className="p-2">{post.content}</blockquote>
   </article>
 );
--- src/components/TimeAgo.tsx
+++ src/components/TimeAgo.tsx
@@ -5,20 +5,19 @@ type TimeAgoProps = PropsWithChildren<{
   when?: string;
   className?: string;
   unit?: DurationUnits;
+  options?: Intl.NumberFormatOptions;
 }>;
 
 export const TimeAgo = ({
   when = DateTime.now().toISO(),
   children,
   unit,
+  options = { unitDisplay: "narrow" },
   className,
 }: TimeAgoProps) => {
   return (
     <time dateTime={when} className={className}>
-      {DateTime.fromISO(when)
-        .diffNow(unit)
-        .negate()
-        .toHuman({ unitDisplay: "narrow" })}
+      {DateTime.fromISO(when).diffNow(unit).negate().toHuman(options)}
       {children || " ago"}
     </time>
   );
